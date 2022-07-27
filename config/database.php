47402072cf6735c98df7df2f09bda7ccd6144a03
RajamakiJuha <110116275+RajamakiJuha@users.noreply.github.com>
2022-07-27 18:28:29 +0300

    add certificate

--- config/database.php
+++ config/database.php
@@ -46,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'bikeapp-server.mysql.database.azure.com'),
             'port' => env('DB_PORT', '3306'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'bike_app_database'),
+            'username'  => env('DB_USERNAME', 'jkhdseuzio'),
+            'password'  => env('DB_PASSWORD', '3UBNG35VE1P21TP8$'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -59,7 +59,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('/home/site/wwwroot/ssl/BaltimoreCyberTrustRoot.crt.pem'),
             ]) : [],
         ],
 
